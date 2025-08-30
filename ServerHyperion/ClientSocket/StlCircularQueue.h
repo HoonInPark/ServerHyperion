d348e4d53cc107498f9bc97cae20bab9d4d618fa
HoonInPark <hooninpark@gmail.com>
2025-08-30 20:43:40 +0900

    modified StlCircularQueue

--- ServerHyperion/ClientSocket/StlCircularQueue.h
+++ ServerHyperion/ClientSocket/StlCircularQueue.h
@@ -8,7 +8,6 @@
 #define SERVERHYPERION_API __declspec(dllimport)
 #endif
 
-#include <iostream>
 #include <cassert>
 #include <atomic>
 #include <vector>
@@ -20,20 +19,13 @@ template<typename T>
 class StlCircularQueue
 {
 public:
-	template <class... P>
+	template<class ...P>
 	StlCircularQueue(size_t buffer_size, P&&... _Params)
+		: buffer_(buffer_size, cell_t(forward<P>(_Params)...))
+		, buffer_mask_(buffer_size - 1)
 	{
-		//assert((buffer_size >= 2) && ((buffer_size & (buffer_size - 1)) == 0));
-		
-		size_t BitCeiled = bit_ceil(buffer_size);
-		buffer_mask_ = BitCeiled - 1;
-		buffer_.resize(BitCeiled);
-
-		for (size_t i = 0; i != BitCeiled; i += 1)
-		{
-			buffer_[i] = make_shared<T>(forward<P>(_Params)...);
+		for (size_t i = 0; i != buffer_size; i += 1)
 			buffer_[i].sequence_.store(i, memory_order_relaxed);
-		}
 
 		enqueue_pos_.store(0, memory_order_relaxed);
 		dequeue_pos_.store(0, memory_order_relaxed);
@@ -41,18 +33,17 @@ public:
 
 	~StlCircularQueue()
 	{
-		// no need to delete for shared_ptr objs...
 	}
 
-	inline bool enqueue(const T& data)
+	bool enqueue(T const& data)
 	{
-		shared_ptr<cell_t> pCell;
+		cell_t* cell;
 		size_t pos = enqueue_pos_.load(memory_order_relaxed);
 
 		for (;;)
 		{
-			pCell = buffer_[pos & buffer_mask_];
-			size_t seq = pCell->sequence_.load(memory_order_acquire);
+			cell = &buffer_[pos & buffer_mask_];
+			size_t seq = cell->sequence_.load(memory_order_acquire);
 			intptr_t dif = (intptr_t)seq - (intptr_t)pos;
 
 			if (dif == 0)		// seq == pos
@@ -72,20 +63,20 @@ public:
 				pos = enqueue_pos_.load(memory_order_relaxed);
 		}
 
-		pCell->data_ = data;
-		pCell->sequence_.store(pos + 1, memory_order_release);
+		cell->data_ = data;
+		cell->sequence_.store(pos + 1, memory_order_release);
 
 		return true;
 	}
 
-	inline bool dequeue(T& data)
+	bool dequeue(T& data)
 	{
-		shared_ptr<cell_t> cell;
+		cell_t* cell;
 		size_t pos = dequeue_pos_.load(memory_order_relaxed);
 
 		for (;;)
 		{
-			cell = buffer_[pos & buffer_mask_];
+			cell = &buffer_[pos & buffer_mask_];
 			size_t seq = cell->sequence_.load(memory_order_acquire);
 			intptr_t dif = (intptr_t)seq - (intptr_t)(pos + 1);
 
@@ -106,27 +97,33 @@ public:
 		return true;
 	}
 
-	void operator = (StlCircularQueue const&);
-
 private:
 	struct cell_t
 	{
+		cell_t() = default;
+
+		template<class ...P>
+		explicit cell_t(P&&... args)
+			: data_(std::forward<P>(args)...)
+		{
+		}
+
 		atomic<size_t>		sequence_;
 		T					data_;
 	};
 
-	vector<shared_ptr<cell_t>> buffer_;
-	size_t				buffer_mask_;
-
+	vector<cell_t>          buffer_;
 	atomic<size_t>			enqueue_pos_;
 	atomic<size_t>			dequeue_pos_;
 
 	static const size_t     cacheline_size = 64;
 	typedef char            cacheline_pad_t[cacheline_size];
 	cacheline_pad_t         pad0_;
+	const size_t            buffer_mask_;
 	cacheline_pad_t         pad1_;
 	cacheline_pad_t         pad2_;
 	cacheline_pad_t         pad3_;
 
-	StlCircularQueue(const StlCircularQueue&);
+	StlCircularQueue(StlCircularQueue const&);
+	void operator = (StlCircularQueue const&);
 };
